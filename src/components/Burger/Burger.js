bfa182dc3e6e18acf5a09ff0b13c8725b0c4387a
Harjot <singh.harjot85@gmail.com>
2020-04-21 22:27:57 -0700

    render burger -- mapObject to array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.css';
-import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
+import classes from "./Burger.css";
+import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredients type = 'bread-top' />
-            <BurgerIngredients type = 'cheese' />
-            <BurgerIngredients type = 'meat' />
-            <BurgerIngredients type = 'bread-bottom' />
-        </div>
-    );
-}
+  const requestedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
+        return [...Array(props.ingredients[ingredientKey])]
+    .map((_, i) => {
+        return <BurgerIngredients key={ingredientKey + i} type={ingredientKey} />;
+    });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top" />
+        {requestedIngredients}
+      <BurgerIngredients type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,18 @@ import React, { Component, Fragment } from "react";
 import Burger from "../../Burger/Burger";
 
 class BurgerBuilder extends Component {
-  state = {};
+  state = {
+      ingredients: {
+          cheese: 1, 
+          salad: 2, 
+          meat: 2, 
+          bacon: 1
+      }
+  };
   render() {
     return (
       <Fragment>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Fragment>
     );
