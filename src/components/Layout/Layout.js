bfb6c3505ca72a3dc651e65f20dd1cdf49abe884
Harjot <singh.harjot85@gmail.com>
2020-05-01 15:58:01 -0700

    Add logo and toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
   <Fragment>
-    <div>Toolbar, Sidebar, Backdrop</div>
+    <Toolbar />
+    <div>Sidebar, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Fragment>
 )
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import burgerLogo from "../../assets/images/original.png"
+
+const logo =(props)=> (
+    <div style={{height: '88%'}}>
+        <img height="48px" width="72px" src={burgerLogo} alt="MyBurgerApp" />
+    </div>
+)
+
+export default logo;
