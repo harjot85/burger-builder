c039d786137558775d48c173f73dd307306b20b2
Harjot <singh.harjot85@gmail.com>
2020-05-05 11:04:11 -0700

    Add side drawer and navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,37 @@
-import React, { Fragment } from "react";
-import classes from './Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import React, { Component, Fragment } from 'react';
 
-const layout = (props) => (
-  <Fragment>
-    <Toolbar />
-    <div>Sidebar, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Fragment>
-)
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-export default layout; 
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
+}
+
+export default Layout;
